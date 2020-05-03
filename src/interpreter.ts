2e970ed2f60faefd6342f8263fbf8ebc9f320aa8
Ludovic L'Hours <ludovic.lhours@gmail.com>
2020-05-03 23:11:28 +0200

    solution step 1.3 : interpreter

--- src/interpreter.ts
+++ src/interpreter.ts
@@ -1,6 +1,32 @@
-import { KaleFile } from "./ast";
+import { KaleFile, Value, ValueType, SimpleValue, Operation, OperationType, BinaryOperation } from "./ast";
 
 export function evaluate(file: KaleFile): string {
-    throw new Error("TODO : complete");
+    const context = new Map<string, any>()
+    file.assignments.forEach(assignment => {
+        context.set(assignment.variable.value, evaluateValue(assignment.value, context));
+    });
+    return String(context.get("message"));
 }
 
+function evaluateValue(value: Value, context: Map<string, any>): any {
+    if (value.type == ValueType.INTEGER) {
+        return (<SimpleValue>value).value;
+    }
+    if (value.type == ValueType.STRING) {
+        return (<SimpleValue>value).value;
+    }
+    if (value.type == ValueType.VARIABLE) {
+        return context.get((<SimpleValue>value).value);
+    }
+    const operation = <BinaryOperation>value;
+    switch(operation.op) {
+        case OperationType.ADD:
+            return evaluateValue(operation.left, context) + evaluateValue(operation.right, context);
+        case OperationType.SUBSTRACT:
+            return evaluateValue(operation.left, context) - evaluateValue(operation.right, context);
+        case OperationType.MULTIPLY:
+            return evaluateValue(operation.left, context) * evaluateValue(operation.right, context);
+        case OperationType.DIVIDE:
+            return evaluateValue(operation.left, context) / evaluateValue(operation.right, context);
+    }
+}
