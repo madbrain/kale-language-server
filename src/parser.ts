0f7b093355a9577718a6369b89f23e8203fa7f46
Ludovic L'Hours <ludovic.lhours@gmail.com>
2020-05-03 22:02:46 +0200

    solution step 1.1 : parser

--- src/parser.ts
+++ src/parser.ts
@@ -1,5 +1,5 @@
 import { Lexer, Token, TokenKind } from "./lexer";
-import { KaleFile } from "./ast";
+import { KaleFile, Assignment, Ident, ValueType, OperationType, Value } from "./ast";
 
 export class Parser {
     private token: Token;
@@ -9,7 +9,69 @@ export class Parser {
     }
 
     parseFile(): KaleFile {
-        throw new Error("TODO : complete");
+        const assignments: Assignment[] = [];
+        while (! this.test(TokenKind.EOF)) {
+            assignments.push(this.parseAssignment());
+        }
+        this.expect(TokenKind.EOF);
+        return { assignments };
+    }
+
+    private parseAssignment(): Assignment {
+        const variable = this.parseIdent();
+        this.expect(TokenKind.ASSIGN);
+        const value = this.parseValue();
+        return { variable, value };
+    }
+
+    private parseIdent(): Ident {
+        const value = this.token.value;
+        this.expect(TokenKind.IDENT);
+        return { value: value! };
+    }
+
+    private parseValue() {
+        let expr: Value = this.parseMulDivValue();
+        while (true) {
+            if (this.test(TokenKind.ADD)) {
+                const right = this.parseMulDivValue();
+                expr = <Value>{ type: ValueType.OPERATION, op: OperationType.ADD, left: expr, right };
+            } else if (this.test(TokenKind.SUBSTRACT)) {
+                const right = this.parseMulDivValue();
+                expr = <Value>{ type: ValueType.OPERATION, op: OperationType.SUBSTRACT, left: expr, right };
+            } else {
+                break;
+            }
+        }
+        return expr;
+    }
+
+    private parseMulDivValue() {
+        let expr: Value = this.parseAtomValue();
+        while (true) {
+            if (this.test(TokenKind.MULTIPLY)) {
+                const right = this.parseAtomValue();
+                expr = <Value>{ type: ValueType.OPERATION, op: OperationType.MULTIPLY, left: expr, right };
+            } else if (this.test(TokenKind.DIVIDE)) {
+                const right = this.parseAtomValue();
+                expr = <Value>{ type: ValueType.OPERATION, op: OperationType.DIVIDE, left: expr, right };
+            } else {
+                break;
+            }
+        }
+        return expr;
+    }
+
+    private parseAtomValue() {
+        const value = this.token.value;
+        if (this.test(TokenKind.INTEGER)) {
+            return { type: ValueType.INTEGER, value: parseInt(value!) };
+        }
+        if (this.test(TokenKind.IDENT)) {
+            return { type: ValueType.VARIABLE, value: value! };
+        }
+        this.expect(TokenKind.STRING);
+        return { type: ValueType.STRING, value: value! };
     }
 
     private scanToken() {
