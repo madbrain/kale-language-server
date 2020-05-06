ee46ba3afc6c0e9f691a1493cee0ad8b161cc71c
Ludovic L'Hours <ludovic.lhours@gmail.com>
2020-05-06 23:24:14 +0200

    solution Step 2.4 : parser robuste

--- src/parser.ts
+++ src/parser.ts
@@ -1,47 +1,79 @@
 import { Lexer, Token, TokenKind } from "./lexer";
-import { KaleFile, Assignment, Ident, ValueType, OperationType, Value } from "./ast";
-import { mergeSpan } from "./positions";
+import { KaleFile, Assignment, Ident, ValueType, OperationType, Value, Operation } from "./ast";
+import { mergeSpan, Span, ErrorReporter } from "./positions";
 
 export class Parser {
     private token: Token;
     
-    constructor(private lexer: Lexer) {
+    constructor(private lexer: Lexer, private reporter: ErrorReporter) {
         this.token = this.scanToken()
     }
 
+    /**
+     * KaleFile ::= Assignment* EOF
+     * 
+     * First(KaleFile) = First(Assignment)
+     */
     parseFile(): KaleFile {
         const start = this.token.span;
         const assignments: Assignment[] = [];
         while (! this.test(TokenKind.EOF)) {
-            assignments.push(this.parseAssignment());
+            this.recoverWith([TokenKind.IDENT, TokenKind.EOF ], start, (endSpan) => {
+                // ignore
+            }, () => {
+                assignments.push(this.parseAssignment());
+            });
         }
         const end = this.token.span;
         this.expect(TokenKind.EOF);
         return { span: mergeSpan(start, end), assignments };
     }
 
+    /**
+     * Assignment ::= Ident ASSIGN Value
+     * 
+     * First(Assignment) = First(Ident) = { IDENT }
+     * Follow(Assignment) = { EOF } + First(Assignment) = { IDENT, EOF }
+     * 
+     */
     private parseAssignment(): Assignment {
         const variable = this.parseIdent();
-        this.expect(TokenKind.ASSIGN);
-        const value = this.parseValue();
-        return { span: mergeSpan(variable.span, value.span), variable, value };
+        return this.recoverWith([ TokenKind.IDENT, TokenKind.EOF], variable.span, (endSpan) => {
+            return <Assignment>{ span: mergeSpan(variable.span, endSpan), isOk: false, variable };
+        }, () => {
+            this.expect(TokenKind.ASSIGN);
+            const value = this.parseValue();
+            return { span: mergeSpan(variable.span, value.span), isOk: true, variable, value };
+        });
     }
 
+    /**
+     * Ident ::= IDENT
+     * 
+     * First(Ident) = { IDENT }
+     * Follow(Ident) = { ASSIGN } + Follow(AtomValue)
+     */
     private parseIdent(): Ident {
         const current = this.token;
         this.expect(TokenKind.IDENT);
         return { span: current.span, value: current.value! };
     }
 
+    /**
+     * Value ::= MulDivValue ( ((ADD | SUBSTRACT) MulDivValue)* | <empty> )
+     * 
+     * First(Value) = First(MulDivValue) = { INTEGER, IDENT, STRING }
+     * Follow(Value) = Follow(Assignment) = { IDENT, EOF }
+     */
     private parseValue(): Value {
+        const SYNC_MUL_DIV_VALUE = [ TokenKind.ADD, TokenKind.SUBSTRACT, TokenKind.IDENT, TokenKind.EOF ];
+        const startSpan = this.token.span;
         let expr: Value = this.parseMulDivValue();
         while (true) {
             if (this.test(TokenKind.ADD)) {
-                const right = this.parseMulDivValue();
-                expr = <Value>{ span: mergeSpan(expr.span, right.span), type: ValueType.OPERATION, op: OperationType.ADD, left: expr, right };
+                expr = this.parseNextValue(OperationType.ADD, expr, SYNC_MUL_DIV_VALUE, startSpan, () => this.parseMulDivValue());
             } else if (this.test(TokenKind.SUBSTRACT)) {
-                const right = this.parseMulDivValue();
-                expr = <Value>{ span: mergeSpan(expr.span, right.span), type: ValueType.OPERATION, op: OperationType.SUBSTRACT, left: expr, right };
+                expr = this.parseNextValue(OperationType.SUBSTRACT, expr, SYNC_MUL_DIV_VALUE, startSpan, () => this.parseMulDivValue());
             } else {
                 break;
             }
@@ -49,15 +81,21 @@ export class Parser {
         return expr;
     }
 
+    /**
+     * MulDivValue ::= AtomValue ( ((MULTIPLY | DIVIDE) AtomValue)* | <empty> )
+     * 
+     * First(MulDivValue) = First(AtomValue) = { INTEGER, IDENT, STRING }
+     * Follow(MulDivValue) = { ADD, SUBSTRACT } + Follow(Value) = { ADD, SUBSTRACT, IDENT, EOF }
+     */
     private parseMulDivValue(): Value {
+        const SYNC_ATOM_VALUE = [ TokenKind.ADD, TokenKind.SUBSTRACT, TokenKind.MULTIPLY, TokenKind.DIVIDE, TokenKind.IDENT, TokenKind.EOF ];
+        const startSpan = this.token.span;
         let expr: Value = this.parseAtomValue();
         while (true) {
             if (this.test(TokenKind.MULTIPLY)) {
-                const right = this.parseAtomValue();
-                expr = <Value>{ span: mergeSpan(expr.span, right.span), type: ValueType.OPERATION, op: OperationType.MULTIPLY, left: expr, right };
+                expr = this.parseNextValue(OperationType.MULTIPLY, expr, SYNC_ATOM_VALUE, startSpan, () => this.parseAtomValue());
             } else if (this.test(TokenKind.DIVIDE)) {
-                const right = this.parseAtomValue();
-                expr = <Value>{ span: mergeSpan(expr.span, right.span), type: ValueType.OPERATION, op: OperationType.DIVIDE, left: expr, right };
+                expr = this.parseNextValue(OperationType.DIVIDE, expr, SYNC_ATOM_VALUE, startSpan, () => this.parseAtomValue());
             } else {
                 break;
             }
@@ -65,6 +103,21 @@ export class Parser {
         return expr;
     }
 
+    private parseNextValue(op: OperationType, expr: Value, syncTokens: TokenKind[], startSpan: Span, parseNext: () => Value) {
+        return this.recoverWith(syncTokens, startSpan, (endSpan) => {
+            return <Value>{ span: mergeSpan(startSpan, endSpan), type: ValueType.OPERATION, isOk: false, op, left: expr };
+        }, () => {
+            const right = parseNext();
+            return <Value>{ span: mergeSpan(expr.span, right.span), type: ValueType.OPERATION, isOk: true, op, left: expr, right };
+        });
+    }
+
+    /**
+     * AtomValue ::= INTEGER | IDENT | STRING
+     * 
+     * First(AtomValue) = { INTEGER, IDENT, STRING }
+     * Follow(AtomValue) = { MULTIPLY, DIVIDE } + Follow(MulDivValue) = { ADD, SUBSTRACT, MULTIPLY, DIVIDE, IDENT, EOF }
+     */
     private parseAtomValue(): Value {
         const current = this.token;
         if (this.test(TokenKind.INTEGER)) {
@@ -92,8 +145,29 @@ export class Parser {
 
     private expect(kind: TokenKind) {
         if (this.token.kind != kind) {
-            throw new Error(`Expecting token ${kind}, got ${this.token.kind}`);
+            const msg = `Expecting ${kind}, got ${this.token.kind}`;
+            this.reporter.reportError(this.token.span, msg)
+            throw new Error(msg);
         }
         this.scanToken();
+    }
+
+    private recoverWith<T>(syncTokens: TokenKind[], start: Span, makeError: (span: Span) => T, parseFunc: () => T): T {
+        try {
+            return parseFunc();
+        } catch(e) {
+            const tokens = this.skipTo(syncTokens);
+            const range = tokens.length > 0 ? mergeSpan(start, tokens[tokens.length-1].span) : start;
+            return makeError(range);
+        }
+    }
+
+    private skipTo(syncTokens: TokenKind[]): Token[] {
+        const tokens: Token[] = []
+        while (! (this.token.kind == TokenKind.EOF || syncTokens.indexOf(this.token.kind) >= 0)) {
+            tokens.push(this.token);
+            this.scanToken();
+        }
+        return tokens;
     }
 }
