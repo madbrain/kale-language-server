53617f73519e1c3cbe5900cdf56e621957688465
Ludovic L'Hours <ludovic.lhours@gmail.com>
2020-05-06 23:24:14 +0200

    solution Step 2.4 : parser robuste

--- src/interpreter.ts
+++ src/interpreter.ts
@@ -1,9 +1,10 @@
-import { KaleFile, Value, ValueType, SimpleValue, Operation, OperationType, BinaryOperation } from "./ast";
+import { KaleFile, Value, ValueType, SimpleValue, Operation, OperationType, BinaryOperation, CompleteAssignment } from "./ast";
 
 export function evaluate(file: KaleFile): string {
     const context = new Map<string, any>()
     file.assignments.forEach(assignment => {
-        context.set(assignment.variable.value, evaluateValue(assignment.value, context));
+        const assign = <CompleteAssignment>assignment;
+        context.set(assign.variable.value, evaluateValue(assign.value, context));
     });
     return String(context.get("message"));
 }
@@ -19,14 +20,16 @@ function evaluateValue(value: Value, context: Map<string, any>): any {
         return context.get((<SimpleValue>value).value);
     }
     const operation = <BinaryOperation>value;
+    const left = evaluateValue(operation.left, context)
+    const right = evaluateValue(operation.right, context);
     switch(operation.op) {
         case OperationType.ADD:
-            return evaluateValue(operation.left, context) + evaluateValue(operation.right, context);
+            return left + right;
         case OperationType.SUBSTRACT:
-            return evaluateValue(operation.left, context) - evaluateValue(operation.right, context);
+            return left - right;
         case OperationType.MULTIPLY:
-            return evaluateValue(operation.left, context) * evaluateValue(operation.right, context);
+            return left * right;
         case OperationType.DIVIDE:
-            return evaluateValue(operation.left, context) / evaluateValue(operation.right, context);
+            return left / right;
     }
 }
